50f2dbffcce53768bab62982a0e136345a1ecea2
darren robinson <darrenrobinsonpt@gmail.com>
2024-07-10 20:26:10 +0000

    Add: Added base and models.

---
+++ workouthub/routes.py
@@ -0,0 +1,8 @@
+from flask import render_template
+from workouthub import app, db
+from workouthub.models import Workout, Exercise 
+
+
+@app.route("/")
+def home():
+    return render_template("base.html") 
