dd3c4694fd1a44e42cbd7082c74b4f6613bddf88
Darren Robinson <darrenrobinsonpt@gmail.com>
2024-07-12 19:37:17 +0000

    Add: added to routes, able to POST to register form

--- workouthub/models.py
+++ workouthub/models.py
@@ -1,10 +1,10 @@
 from workouthub import db
 
-class Users(db.Model):
+class User(db.Model):
     # Schema for the users model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     workouts = db.relationship("Workout", backref="user", cascade="all, delete", lazy=True)
 
     def __repr__(self):
@@ -13,7 +13,7 @@ class Users(db.Model):
 class Workout(db.Model):
     # Schema for the workout model
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
     workout_name = db.Column(db.String(90), unique=True, nullable=False)
     workout_type = db.Column(db.String(90), nullable=False) 
     exercises = db.relationship("Exercise", backref="workout", cascade="all, delete", lazy=True)
--- workouthub/routes.py
+++ workouthub/routes.py
@@ -1,12 +1,44 @@
-from flask import render_template
+from flask import (
+    flash, render_template,
+    redirect, request, session, url_for)
+from werkzeug.security import generate_password_hash, check_password_hash
 from workouthub import app, db
-from workouthub.models import Workout, Exercise 
+from workouthub.models import User, Workout, Exercise
 
 
 @app.route("/")
 def home():
-    return render_template("home.html") 
+    return render_template("home.html")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        
+        if not username or not password:
+            flash("Username and password are required")
+            return redirect(url_for("register"))
+
+        username = username.lower()
+
+        # Checking to see if username is unique
+        existing_user = User.query.filter_by(username=username).first()
+
+        if existing_user:
+            flash("Username already in use")
+            return redirect(url_for("register"))
+
+        hashed_password = generate_password_hash(password)
+        new_user = User(username=username, password=hashed_password)
+        
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Put the new user into session
+        session["user"] = username
+        flash("Registration successful")
+        return redirect(url_for("home"))
+
     return render_template("register.html")
