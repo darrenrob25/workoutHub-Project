a57d79fbaa0a832f41a35141a31935a178dd53c2
Darren Robinson <darrenrobinsonpt@gmail.com>
2024-07-12 18:58:43 +0000

    Add: Built register page, got link working to page

--- workouthub/routes.py
+++ workouthub/routes.py
@@ -6,3 +6,7 @@ from workouthub.models import Workout, Exercise
 @app.route("/")
 def home():
     return render_template("home.html") 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
