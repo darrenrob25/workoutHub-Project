940752bee8b886af4db486f099e424620d7c11ae
Darren Robinson <darrenrobinsonpt@gmail.com>
2024-07-14 15:33:51 +0000

    Add: Formatting and comments to init file

--- workouthub/__init__.py
+++ workouthub/__init__.py
@@ -1,12 +1,22 @@
+# Importing modules and functions
 import os 
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# Check if the env.py file exists for environment settings
 if os.path.exists("env.py"):
     import env
+
+# Importing QueuePool class from sqlalchemy
 from sqlalchemy.pool import QueuePool
 
+
+# Create the flask application
 app = Flask(__name__)
+
+# Configure the flask app with the secret key
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
